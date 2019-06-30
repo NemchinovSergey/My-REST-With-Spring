f5655b53e31bb054fc2b0016255f2810db74514d
Nemchinov Sergey <googoodemon@gmail.com>
2019-06-30 22:33:58 +0700

    spring web configuration class

---
+++ src/main/java/com/nsergey/spring/WebConfig.java
@@ -0,0 +1,12 @@
+package com.nsergey.spring;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.nsergey.web")
+public class WebConfig {
+
+}

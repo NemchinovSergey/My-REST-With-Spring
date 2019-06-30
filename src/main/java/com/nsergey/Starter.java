bb58ad2b251fde90e8266cc7b9979cfec7dca97e
Nemchinov Sergey <googoodemon@gmail.com>
2019-06-30 21:30:52 +0700

    main method and logger

--- src/main/java/com/nsergey/Starter.java
+++ src/main/java/com/nsergey/Starter.java
@@ -1,4 +1,13 @@
 package com.nsergey;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Starter {
+    private static final Logger logger = LoggerFactory.getLogger(Starter.class);
+
+    public static void main(String[] args) {
+        logger.info("STARTED");
+    }
+
 }

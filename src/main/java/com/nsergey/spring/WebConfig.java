6373cb77649c1e1d4be05ad5ae7ea66bbfe5bb16
Nemchinov Sergey <googoodemon@gmail.com>
2019-06-30 22:39:41 +0700

    removed unnecessary parameter name

--- src/main/java/com/nsergey/spring/WebConfig.java
+++ src/main/java/com/nsergey/spring/WebConfig.java
@@ -6,7 +6,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackages = "com.nsergey.web")
+@ComponentScan("com.nsergey.web")
 public class WebConfig {
 
 }
